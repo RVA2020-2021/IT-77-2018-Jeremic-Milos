2c6e0d7066b5ccfbf74da93edd46013a53b1e38e
jeremicmilos <mikijeremickl@gmail.com>
2021-06-08 11:16:50 +0200

    Test 2

---
+++ src/app/app.constants.ts
@@ -0,0 +1,6 @@
+export const OBRAZOVANJE_URL = 'http://localhost:8084/obrazovanje';
+export const PREDUZECE_URL = 'http://localhost:8084/preduzece';
+export const SEKTOR_URL = 'http://localhost:8084/sektor';
+export const RADNIK_URL = 'http://localhost:8084/radnik';
+export const RADNIK_SEKTOR_URL = 'http://localhost:8084/radnikSektor';
+
